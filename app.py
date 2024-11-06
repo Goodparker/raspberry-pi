805bf0ad8f03a9b7530fc1d62fc273e54da194bc
MarkIbragimovRIS21-1b <92600879+MarkIbragimovRIS21-1b@users.noreply.github.com>
2024-11-06 12:45:29 +0500

    Update app.py
    Добавление в код функции для сервопривода, позволяющей ему запускаться на 3 секунды.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 import RPi.GPIO as GPIO
+import time
 
 app = Flask(__name__)
 
-# Настройка GPIO
-LIGHT_PIN = 18  # Укажите номер пина
+LIGHT_PIN = 18
+SERVO_PIN = 17  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LIGHT_PIN, GPIO.OUT)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
 
 @app.route('/light/<state>', methods=['GET'])
 def toggle_light(state):
@@ -14,6 +16,14 @@ def toggle_light(state):
         GPIO.output(LIGHT_PIN, GPIO.HIGH)
     elif state == 'off':
         GPIO.output(LIGHT_PIN, GPIO.LOW)
+    return '', 204
+
+@app.route('/servo/<state>', methods=['GET'])
+def toggle_servo(state):
+    if state == 'on':
+        GPIO.output(SERVO_PIN, GPIO.HIGH) 
+        time.sleep(3)                       
+        GPIO.output(SERVO_PIN, GPIO.LOW)   
     return '', 204
 
 if __name__ == '__main__':
