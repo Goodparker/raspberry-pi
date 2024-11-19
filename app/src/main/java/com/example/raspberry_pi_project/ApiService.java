1435e01c7131f89c15c39129aa43c0ab92a0bc4c
Parker <parker1202002@gmail.com>
2024-11-19 15:21:15 +0500

    add android app

---
+++ app/src/main/java/com/example/raspberry_pi_project/ApiService.java
@@ -0,0 +1,10 @@
+package com.example.raspberry_pi_project;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ApiService {
+    @GET("/light/{state}")
+    Call<Void> toggleLight(@Path("state") String state);
+}
---
+++ app/src/main/java/com/example/raspberry_pi_project/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.raspberry_pi_project;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnLightOn, btnLightOff;
+    private ApiService apiService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLightOn = findViewById(R.id.btnOn);
+        btnLightOff = findViewById(R.id.btnOff);
+
+        apiService = ApiClient.getClient().create(ApiService.class);
+
+        btnLightOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleLight("on");
+            }
+        });
+
+        btnLightOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleLight("off");
+            }
+        });
+    }
+
+    private void toggleLight(String state) {
+        Call<Void> call = apiService.toggleLight(state);
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Light turned " + state, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
